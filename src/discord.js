6876fd0d0a8f1b2c5d55fe59073d753573822583
Halo333X <gaelorian110705@gmail.com>
2024-07-28 23:56:36 -0600

    xd

--- src/discord.js
+++ src/discord.js
@@ -3,13 +3,23 @@ const clientId = "1267325685844803646"; // Reemplaza con tu Client ID de Discord
 
 const rpc = new RPC.Client({ transport: "ipc" });
 
-rpc.on("ready", () => {
+rpc.on("ready", async () => {
+  const data = await fetch(
+    "https://raw.githubusercontent.com/IgnitedsStudios/IgnitedsLauncher/main/src/event.json"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error("Error al cargar el archivo JSON:", error);
+    });
   rpc.setActivity({
-    smallImageText: "Jugando Squid Bedrock 2!",
-    state: "Jugando Squid Bedrock 2!",
+    smallImageText: data ? data.discord_rpc_event : 'Idle!',
+    state: data ? data.discord_rpc_event : 'Idle!',
     startTimestamp: new Date(), // El tiempo actual cuando comienza la actividad
     largeImageKey: "logo", // Nombre de la imagen grande configurada en Discord
-    largeImageText: "Jugando Squid Bedrock 2!", // Texto que se muestra cuando se pasa el ratón sobre la imagen grande
+    largeImageText: data ? data.discord_rpc_event : 'Idle!', // Texto que se muestra cuando se pasa el ratón sobre la imagen grande
     instance: false,
   });
 });
